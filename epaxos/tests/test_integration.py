300dbfddc04be573409c8efe4eec2a437484d461
Blake Eggleston <bdeggleston@gmail.com>
2014-09-23 17:25:29 -0700

    adding a variation of the inferred fast path commit recovery test

--- epaxos/tests/test_integration.py
+++ epaxos/tests/test_integration.py
@@ -214,9 +214,27 @@ class EpaxosIntegrationTest(EpaxosTestCase):
             self.assertEqual(leader1.instances, {})
             self.assertEqual(leader2.instances, {})
 
-    def test_inferred_fast_path_failure_recovery(self):
+    def test_inferred_fast_path_failure_recovery_failed_leader(self):
         """
-        :return:
+        Tests failure recovery in the following scenario
+
+        1: Assuming a cluster with replicas numbered 0-N. Replica N attempts to run an instance
+        with only the fast quorum minority responding (just itself for RF3, itself and another for
+        RF5, and so on). The query is aborted since a basic quorum didn't respond.
+
+        2: Replica 0 runs an instance of paxos. It receives identical attributes from a fast path quorum,
+        which are also the nodes that weren't reached in query 1. The minority nodes receive and update
+        the preaccept response, but replica 0 doesn't receive them. Upon receiving a fast path quorum of
+        identical responses, it commits and executes the query, responds with success to the client, and
+        asyncronously notifies all other replicas of the commit. However, None of the other nodes receive
+        the commit.
+
+        3: Replica N attempts to run an instance. It can still contact the other minority node from step 1
+        (if any), and can also contact (only) one of the non-leader replicas from step 2, which never received
+        a commit message for step 2. When replica N goes to commit and execute this new instance, it should run
+        a prepare phase for the instances from step 1 & 2. It MUST commit the instance from step 2 with no
+        dependencies. The instance from step 1 may be committed with a noop, but should come after the instance
+        from step 2 in the execution order. The instance from step 3 should be executed last.
         """
         # first, preaccept an instance on a fast path minority of the nodes
         self.set_replica_state(self.replicas[:self.fast_path_quorum_size], RS.DOWN)
@@ -289,6 +307,72 @@ class EpaxosIntegrationTest(EpaxosTestCase):
         self.check_execution_order(self.replicas[-self.fast_path_quorum_size:], expected_order)
         self.check_execution_order(self.replicas, expected_order, allow_unexecuted=True)
 
+    def test_inferred_fast_path_failure_recovery_new_leader(self):
+        """
+        same scenario as `test_inferred_fast_path_failure_recovery_failed_leader`, except the leader for
+        the third step is on of the non-leader replicas from step 2.
+        """
+        # first, preaccept an instance on a fast path minority of the nodes
+        self.set_replica_state(self.replicas[:self.fast_path_quorum_size], RS.DOWN)
+        self.next_leader = len(self.replicas) - 1
+        leader1 = self.get_leader()
+        with self.assertRaises(QuorumFailure):
+            leader1.coordinate_request(self.replicas, nickname="failed_instance")
+        failed_instance = leader1.last_created_instance
+        self.assertTrue(failed_instance.fast_path_impossible)
+        self.check_replicas_instance(failed_instance, set(), self.replicas[self.fast_path_quorum_size:], IS.PREACCEPTED)
+        self.check_replicas_instance_unseen(failed_instance, self.replicas[:self.fast_path_quorum_size])
+
+        # second, a fast path of replicas responds with identical attributes, but the fast path minority responds with
+        # non matching attributes. The leader doesn't receive the dissenting responses, resulting in a fast path commit,
+        # but the non-leaders don't receive the commit message
+        self.next_leader = 0
+        leader2 = self.get_leader()
+        self.set_replica_state(self.replicas, RS.UP)
+        self.set_replica_state(self.replicas[self.fast_path_quorum_size:], RS.NORESPONSE)
+        def second_step_commit_hook():
+            self.set_replica_state(self.replicas[1:], RS.DOWN)
+        leader2.coordinate_request(self.replicas, commit_hook=second_step_commit_hook, nickname="committed_instance")
+        committed_instance = leader2.last_created_instance
+        committed_instance_deps = set()
+        # 1st instance should be committed
+        self.check_replicas_instance(committed_instance, committed_instance_deps, self.replicas[:1], state=IS.EXECUTED)
+
+        # 2nd through fast path quorum should be preaccepted with no deps
+        self.check_replicas_instance(committed_instance, set(), self.replicas[1:self.fast_path_quorum_size], state=IS.PREACCEPTED)
+
+        # fast path minority should be preaccepted with failed instance dep
+        self.check_replicas_instance(committed_instance, {failed_instance.iid}, self.replicas[self.fast_path_quorum_size:], state=IS.PREACCEPTED)
+
+        # third, switch the partition, so all nodes that contributed to the second commit are down, with the exception
+        # of one which didn't commit, and run an instance. When this instance is executed, the uncommitted instance from
+        # the previous run will be prepared, and the prepare stage should be able to infer that the second instance was
+        # committed on the fast path, and shouldn't restart the preaccept phase
+        self.set_replica_state(self.replicas, RS.DOWN)
+        self.set_replica_state(self.replicas[-self.fast_path_quorum_size:], RS.UP)
+        self.next_leader = self.fast_path_quorum_size - 1
+        leader3 = self.get_leader()
+        assert leader3.state == RS.UP
+        leader3.coordinate_request(self.replicas, nickname="successful_instance")
+        successful_instance = leader3.last_created_instance
+
+        # instance was committed on replica 0 with no deps, and it must be here
+        self.check_replicas_instance(committed_instance,
+                                     committed_instance_deps,
+                                     [self.replicas[0]] + self.replicas[-self.fast_path_quorum_size:],
+                                     state=(IS.EXECUTED, IS.COMMITTED))
+        self.check_replicas_instance(failed_instance,
+                                     {committed_instance, successful_instance},
+                                     self.replicas[-self.fast_path_quorum_size:],
+                                     state=(IS.EXECUTED, IS.COMMITTED))
+        self.check_replicas_instance(successful_instance,
+                                     {committed_instance, failed_instance},
+                                     self.replicas[-self.fast_path_quorum_size:],
+                                     state=(IS.EXECUTED, IS.COMMITTED))
+
+        expected_order = [committed_instance, failed_instance, successful_instance]
+        self.check_execution_order(self.replicas[-self.fast_path_quorum_size:], expected_order)
+        self.check_execution_order(self.replicas, expected_order, allow_unexecuted=True)
 
 class EpaxosIntegrationTestReplicaLeaderRF3(EpaxosIntegrationTest):
     replication_factor = 3
